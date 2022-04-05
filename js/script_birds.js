e300e1ca6ea56ec4877c4b52ab193083c7bab781
Catsarinka <97660998+Catsarinka@users.noreply.github.com>
2022-04-05 15:23:40 +0300

    Update script_birds.js

--- js/script_birds.js
+++ js/script_birds.js
@@ -1,18 +1,12 @@
-// Получить модель
+// МОДАЛЬНОЕ ОКНО
 var modal = document.getElementById("myModal");
 
-// Получите изображение и вставьте его внутрь модального 
 var img = document.getElementById("myImg");
 var modalImg = document.getElementById("img01");
 
 var img1 = document.getElementById("myImg1");
 var img2 = document.getElementById("myImg2");
-// var img3 = document.getElementById("myImg3");
-// var img4 = document.getElementById("myImg4");
-// var img5 = document.getElementById("myImg5");
-// var img6 = document.getElementById("myImg6");
-// var img7 = document.getElementById("myImg7");
-// var img8 = document.getElementById("myImg8");
+
 
 //ФУНКЦИИ
 
