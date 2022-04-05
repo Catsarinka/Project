5e989587c1dac14ea0d1cce2d084b0619384dea2
Catsarinka <97660998+Catsarinka@users.noreply.github.com>
2022-04-05 15:27:53 +0300

    Update script_winter.js

--- js/script_winter.js
+++ js/script_winter.js
@@ -6,12 +6,7 @@ var modalImg = document.getElementById("img01");
 
 var img1 = document.getElementById("myImg1");
 var img2 = document.getElementById("myImg2");
-// var img3 = document.getElementById("myImg3");
-// var img4 = document.getElementById("myImg4");
-// var img5 = document.getElementById("myImg5");
-// var img6 = document.getElementById("myImg6");
-// var img7 = document.getElementById("myImg7");
-// var img8 = document.getElementById("myImg8");
+
 
 //ФУНКЦИИ
 
